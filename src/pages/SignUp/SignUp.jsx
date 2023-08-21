f409425b4d6d6116f4e1399532c578c2a8848d2a
Ruslaana Onyshchuk <lalelya7@gmail.com>
2023-08-21 21:13:51 +0200

    deleted unnecessary files

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -44,9 +44,6 @@ const SignUpPage = () => {
             alignItems: 'center',
           }}
         >
-          {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar> */}
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
